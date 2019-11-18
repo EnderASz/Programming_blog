42b7512a3df90fca6977ec62a93feb7fc8cdbbbe
EnderASz <olek5188@gmail.com>
2019-11-18 22:35:42 +0100

    Added reference from 'assets/html/head_tag_std.html' to 'assets/html/load_fonts.html' Move 'TEMPLATES' settings from 'settings_components/base.py' to 'settings_components/static.py'

--- blog/blog/settings_components/base.py
+++ blog/blog/settings_components/base.py
@@ -39,24 +39,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'blog.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, "assets/html"),
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'blog.wsgi.application'
 
 
--- blog/blog/settings_components/static.py
+++ blog/blog/settings_components/static.py
@@ -1,6 +1,11 @@
-# Static files (CSS, JavaScript, Images)
+# Static files (HTML Templates, CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+include(
+    'base.py',
+)
+
+# CSS, JavaScript, Images
 STATICFILES_FINDERS = []
 
 STATICFILES_DIRS = [
@@ -8,3 +13,22 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/assets/'
 STATIC_ROOT = ""
+
+#HTML TEMPLATES
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, "assets\\html"),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
