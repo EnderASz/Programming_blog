00919834818c258d0dba3a4bd5e12fadc6d004f0
EnderASz <olek5188@gmail.com>
2019-11-18 23:25:34 +0100

    Moved database's settings to 'settings_components/database.py' and changed database type to MySQL

--- blog/blog/settings_components/base.py
+++ blog/blog/settings_components/base.py
@@ -41,18 +41,6 @@ ROOT_URLCONF = 'blog.urls'
 
 WSGI_APPLICATION = 'blog.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
---
+++ blog/blog/settings_components/database.py
@@ -0,0 +1,11 @@
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'db.cnf')
+        }
+    }
+}
