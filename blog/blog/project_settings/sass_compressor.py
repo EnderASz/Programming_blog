7066b4b66d64609347ffc633b9322ebcc76dd0f6
EnderASz <olek5188@gmail.com>
2019-12-04 15:34:22 +0100

    cache for pythonanywhere

--- blog/blog/project_settings/sass_compressor.py
+++ blog/blog/project_settings/sass_compressor.py
@@ -10,7 +10,7 @@ STATICFILES_FINDERS += [
     'compressor.finders.CompressorFinder',
 ]
 
-COMPRESS_ROOT = 'assets/'
+COMPRESS_ROOT = 'Programming_blog/blog/assets/'
 
 COMPRESS_PRECOMPILERS = [
     ('text/x-scss', 'django_libsass.SassCompiler'),
