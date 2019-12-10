66e0500f8cebe8ca0fd678353c568b52ea1d2b3f
EnderASz <olek5188@gmail.com>
2019-12-10 17:31:57 +0100

    fix settings directories

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -1,9 +1,9 @@
 from split_settings.tools import include
 
 include(
-    'project_settings\\base.py',
-    'project_settings\\my_apps.py',
-    'project_settings\\static.py',
-    'project_settings\\sass_compressor.py',
-    'project_settings\\database.py',
+    'project_settings/base.py',
+    'project_settings/my_apps.py',
+    'project_settings/static.py',
+    'project_settings/sass_compressor.py',
+    'project_settings/database.py',
 )
