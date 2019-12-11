558db0b9a5b75ad59000b38cc62e85a5b9038edb
EnderASz <olek5188@gmail.com>
2019-12-11 09:05:46 +0100

    cache for pythonanywhere

--- blog/apps/start_pages/views.py
+++ blog/apps/start_pages/views.py
@@ -4,18 +4,12 @@ from django.conf import settings
 from apps.posts_manager.models import Post
 from apps.tags_manager.models import Tag
 
-from assets.python import std_template_loads
-
-import os
+from assets.python import std_template_funcs
 
 def start_page(request, page_number=0):
-    print(page_number)
     max_posts_per_page = settings.POSTSLISTING['MAX_POSTS_PER_PAGE']
-    page_number = request.POST.get('page_index', 0)
     first_index = page_number*max_posts_per_page
     last_index = first_index+max_posts_per_page
-
-    test = os.path.join(settings.BASE_DIR, 'db.sqlite3')
 
     all_posts = Post.objects.order_by('-date_add')
     posts_wrap = all_posts[first_index:last_index]
@@ -25,10 +19,14 @@ def start_page(request, page_number=0):
         'standard_template.html',
             {
                 'title': "Strona Startowa",
-                'page_destination' : 'start-page',
+                'page_destinations' : ['start-page', 'posts_list'],
                 'avaible_tags' : std_template_loads.get_all_tags(),
                 'recent_posts' : std_template_loads.get_recent_posts(all_posts),
-                'display_content': ['post_wrapper'],
+                'display_content': ['post_wrapper', 'page_switcher'],
                 'posts_wrap': posts_wrap,
+                'page_info': {
+                    'pages_amount': all_posts.count()//max_posts_per_page,
+                    'current_page': page_number,
+                }
             }
     )
