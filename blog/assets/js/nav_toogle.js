ea6e99d3c7e57ac2e7b911e06f5bad20285b89df
EnderASz <olek5188@gmail.com>
2019-12-14 15:51:24 +0100

    cached for pythonanywhere

--- blog/assets/js/nav_toogle.js
+++ blog/assets/js/nav_toogle.js
@@ -1,4 +1,6 @@
-document.getElementById("nav_toogle_bt").addEventListener("click", function(){
-    var nav_list = document.getElementById("nav_list");
-    nav_list.classList.add("unrolled")
-})
+window.onload = function(){
+    document.getElementById("nav_toogle_bt").addEventListener("click", function(){
+        var nav_list = document.getElementById("nav_list");
+        nav_list.classList.toogle("unrolled");
+    });
+};
