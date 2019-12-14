a052bc1cb5cd045c84c1b60ebd6eb1e9a0fede3a
EnderASz <olek5188@gmail.com>
2019-12-14 16:23:36 +0100

    cached for pythonanywhere

--- blog/assets/js/main.js
+++ blog/assets/js/main.js
@@ -1,8 +1,10 @@
-window.onload += function(){
-    window.alert("test");
+window.onload = function() {
+
+    document.querySelector('body').classList.remove('preload');
+
     document.getElementById("nav_toogle_bt").addEventListener("click", function(){
         window.alert("tes1t");
         var nav_list = document.getElementById("nav_list");
         nav_list.classList.toogle("unrolled");
     });
-};
+}
