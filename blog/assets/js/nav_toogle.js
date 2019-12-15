e21736a99697e94e94aa1f4d1db5651860e24170
EnderASz <olek5188@gmail.com>
2019-12-15 13:56:23 +0100

    cached for pythonanywhere

--- blog/assets/js/nav_toogle.js
+++ blog/assets/js/nav_toogle.js
@@ -1,4 +1,4 @@
-document.getElementById("nav_toogle_bt").addEventListener("click", function(){
+function toogle_bar() {
     var nav_list = document.getElementById("nav_list");
     nav_list.classList.toogle("unrolled");
-});
+}
