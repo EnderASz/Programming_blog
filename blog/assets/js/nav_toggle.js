cfce5b398d57ac9e9957e649c22e31e88f25dffc
EnderASz <olek5188@gmail.com>
2019-12-15 14:00:21 +0100

    cached for pythonanywhere

---
+++ blog/assets/js/nav_toggle.js
@@ -0,0 +1,4 @@
+function toggle_bar() {
+    var nav_list = document.getElementById("nav_list");
+    nav_list.classList.toggle("unrolled");
+}
--- blog/assets/js/nav_toogle.js
+++
@@ -1,4 +0,0 @@
-function toogle_bar() {
-    var nav_list = document.getElementById("nav_list");
-    nav_list.classList.toogle("unrolled");
-}
