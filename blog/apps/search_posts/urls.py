ffc2913830e634aa5e022b46ef27402634eece99
EnderASz <olek5188@gmail.com>
2019-12-17 20:35:12 +0100

    cached for pythonanywhere

--- blog/apps/search_posts/urls.py
+++ blog/apps/search_posts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('/tag/<slug:tag_slug>', views.tag_search, name="search-by-tag"),
-    path('author/<slug:author_slug>', views.author_search, name="search-by-author"),
+    path('/author/<slug:author_slug>', views.author_search, name="search-by-author"),
     path('/tag/<slug:tag_slug>/<int:page_number>', views.tag_search, name="search-by-tag_int"),
-    path('author/<slug:author_slug>/<int:page_number>', views.author_search, name="search-by-author_int"),
+    path('/author/<slug:author_slug>/<int:page_number>', views.author_search, name="search-by-author_int"),
 ]
