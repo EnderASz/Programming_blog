9b75173ae6bedbc67fec0a854288c3e959ab4347
EnderASz <olek5188@gmail.com>
2019-12-17 19:29:28 +0100

    cached for pythonanywhere

--- blog/apps/search_posts/urls.py
+++ blog/apps/search_posts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/tag/<slug:tag_slug>', views.tag_search, name="search-by-tag"),
-    path('/author/<slug:author_slug>', views.author_search, name="search-by-author"),
-    path('/tag/<slug:tag_slug>/<int:page_number>', views.tag_search, name="search-by-tag_int"),
-    path('/author/<slug:author_slug>/<int:page_number>', views.author_search, name="search-by-author_int"),    
+    path('tag/<slug:tag_slug>', views.tag_search, name="search-by-tag"),
+    path('author/<slug:author_slug>', views.author_search, name="search-by-author"),
+    path('tag/<slug:tag_slug>/<int:page_number>', views.tag_search, name="search-by-tag_int"),
+    path('author/<slug:author_slug>/<int:page_number>', views.author_search, name="search-by-author_int"),    
 ]
