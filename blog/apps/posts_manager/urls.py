acbf4c6f78ce0529b8eb625cfecc6c4b121a1d3b
EnderASz <olek5188@gmail.com>
2019-12-17 20:27:38 +0100

    cached for pythonanywhere

--- blog/apps/posts_manager/urls.py
+++ blog/apps/posts_manager/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<slug:post_slug>', views.post_view, name='post_view'),
+    path('/<slug:post_slug>', views.post_view, name='post_view'),
 ]
